28e1a9857a1d002521baa041574062f109592e7a
rina1309 <113968192+rina1309@users.noreply.github.com>
2023-11-22 11:19:02 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -23,18 +23,18 @@ export default function Form() {
   }
 
   return (
-    <div className="registration-form">
+    <div className="registration_form">
       <h1>Регистрация пользователя</h1>
       <form>
-        <div className="form-group">
+        <div className="form_group">
           <label class="fullName">ФИО</label>
           <input type="text" id="firstName" name="firstName" value={formData.fullName} onChange={handleChange}/>
         </div>
-        <div className="form-group">
+        <div className="form_group">
           <label class="phoneNumber">Номер телефона</label>
           <input type="tel" id="phoneNumber" name="phoneNumber" value={formData.phoneNumber} onChange={handleChange}/>
         </div>
-        <div className="form-group">
+        <div className="form_group">
           <label class="email">Email адрес</label>
           <input type="email" id="email" name="email" value={formData.email} onChange={handleChange}/>
         </div>
